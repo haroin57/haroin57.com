808ae4b789631f1f35f240528abd0a7745c21cbe
harut <harutomann2003@gmail.com>
2025-12-31 14:59:10 +0900

    記事更新

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -1,6 +1,73 @@
 import { useEffect, useRef, memo } from 'react'
 import { useMermaidBlocks } from '../hooks/useMermaidBlocks'
 
+// Twitter埋め込みウィジェットの型定義
+type TwitterWindow = Window & {
+  twttr?: {
+    widgets?: {
+      load?: (el?: HTMLElement) => void
+    }
+    ready?: (callback: () => void) => void
+  }
+}
+
+// Twitter埋め込みウィジェットをロード
+function useTwitterEmbeds(containerRef: React.RefObject<HTMLDivElement | null>, deps: unknown[]) {
+  useEffect(() => {
+    const container = containerRef.current
+    if (!container) return
+
+    // Twitter埋め込みがあるか確認
+    const twitterEmbeds = container.querySelectorAll('.twitter-tweet')
+    if (twitterEmbeds.length === 0) return
+
+    const win = window as TwitterWindow
+
+    // ウィジェットをロードする関数
+    const loadWidgets = () => {
+      if (win.twttr?.widgets?.load) {
+        win.twttr.widgets.load(container)
+      }
+    }
+
+    // window.twttr が既に存在する場合はウィジェットをロード
+    if (win.twttr?.widgets?.load) {
+      loadWidgets()
+      return
+    }
+
+    // スクリプトがまだない場合は追加
+    const existingScript = document.querySelector('script[src*="platform.twitter.com/widgets.js"]')
+    if (!existingScript) {
+      const script = document.createElement('script')
+      script.src = 'https://platform.twitter.com/widgets.js'
+      script.async = true
+      script.setAttribute('charset', 'utf-8')
+      script.onload = () => {
+        // スクリプトロード後、twttr.ready を使うか直接ロード
+        if (win.twttr?.ready) {
+          win.twttr.ready(loadWidgets)
+        } else {
+          // フォールバック: 少し待ってからロード
+          setTimeout(loadWidgets, 100)
+        }
+      }
+      document.body.appendChild(script)
+    } else {
+      // スクリプトは存在するがまだロード中の場合
+      const checkAndLoad = () => {
+        if (win.twttr?.widgets?.load) {
+          loadWidgets()
+        } else {
+          setTimeout(checkAndLoad, 100)
+        }
+      }
+      checkAndLoad()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps)
+}
+
 // HTMLコンテンツをメモ化したコンポーネント
 // 親コンポーネントの再レンダリングでMermaidのSVGが消えるのを防ぐ
 const PostContent = memo(function PostContent({
@@ -18,6 +85,7 @@ const PostContent = memo(function PostContent({
   }, [onProseRef])
 
   useMermaidBlocks(proseRef, [html])
+  useTwitterEmbeds(proseRef, [html])
 
   return (
     <div
